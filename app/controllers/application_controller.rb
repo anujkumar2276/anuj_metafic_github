373bd447ee02d7dd87b6d093392d85a4f5e03d8f
Anuj Kumar <anuj.kumar@metafic.co>
2023-02-07 23:57:58 +0530

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :null_session
+    include JsonWebToken
+
+    before_action :authenticate_request
+
+    private
+        def authenticate_request
+            header = request.headers["Authorization"]
+            header = header.split(" ").last if header
+            decoded = jwt_decode(header)
+            @current_user = User.find(decoded[:user_id])
+        end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    require "securerandom"
+
+    has_secure_password
+
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :name, presence: true, uniqueness: true
+end
